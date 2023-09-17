a958c9d2513909c08ba143b9db63482a01e77750
HyunggiPark <phg9587@naver.com>
2023-09-17 20:03:31 +0900

    feat: Logout 설정 추가

--- src/main/java/com/example/preventduplicatelogin/config/SecurityConfig.java
+++ src/main/java/com/example/preventduplicatelogin/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.preventduplicatelogin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
@@ -39,7 +38,14 @@ public class SecurityConfig {
                                 .permitAll() //Login 관련 페이지에 접근할때는 Authenticate 하지 않도록 하는 설정
                 )
 
-                .logout(Customizer.withDefaults())
+                .logout(logout ->
+                        logout
+                                .logoutUrl("/logout")  /* 로그아웃 url*/
+                                .logoutSuccessUrl("/login")  /* 로그아웃 성공시 이동할 url */
+                                .invalidateHttpSession(true)  /*로그아웃시 세션 제거*/
+                                .deleteCookies("JSESSIONID")  /*쿠키 제거*/
+                                .clearAuthentication(true)    /*권한정보 제거*/
+                                .permitAll())
 
                 .sessionManagement(sm ->
                         sm
