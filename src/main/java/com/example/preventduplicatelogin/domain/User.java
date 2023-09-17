1ee428b468c1f119ffc5a566bc543965a024d2d2
HyunggiPark <phg9587@naver.com>
2023-09-17 16:37:33 +0900

    feat: User 도메인 추가

---
+++ src/main/java/com/example/preventduplicatelogin/domain/User.java
@@ -0,0 +1,16 @@
+package com.example.preventduplicatelogin.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+    @Column(name = "id")
+    private String userId;
+    private String password;
+    private LocalDateTime regDate;
+}
