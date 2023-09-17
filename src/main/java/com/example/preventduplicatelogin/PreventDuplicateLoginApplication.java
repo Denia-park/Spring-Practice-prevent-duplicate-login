0ed8daa16fa9569c9bfddde9cd53b0e7351b9353
HyunggiPark <phg9587@naver.com>
2023-09-17 15:38:27 +0900

    init

---
+++ src/main/java/com/example/preventduplicatelogin/PreventDuplicateLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.preventduplicatelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventDuplicateLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreventDuplicateLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/preventduplicatelogin/PreventDuplicateLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preventduplicatelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventDuplicateLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
